9a7c12d8806a0e2eeb6e88bce41825ccdcd0e49b
Daniele Checchin <danielechecchin26@gmail.com>
2026-06-09 09:59:24 +0200

    add categories navigation to layout

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -40,6 +40,12 @@ new class extends Component
                         {{ __('Account') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.index')" wire:navigate>
+                        {{ __('Categorie') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-Route::view('accounts', 'livewire.accounts.index')
-    ->middleware(['auth'])
-    ->name('accounts.index');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('profile', 'profile')->name('profile');
+    Route::view('accounts', 'livewire.accounts.index')->name('accounts.index');
+    Route::view('categories', 'livewire.categories.index')->name('categories.index');
+});
 
 
 
