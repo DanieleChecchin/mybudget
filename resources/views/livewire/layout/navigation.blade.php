c96a590a3cbc556e90a2d1188903d71c1576f3b1
Daniele Checchin <danielechecchin26@gmail.com>
2026-06-09 10:02:04 +0200

    fix responsive navigation links

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -96,6 +96,14 @@ new class extends Component
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('accounts.index')" :active="request()->routeIs('accounts.index')" wire:navigate>
+                {{ __('Account') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.index')" wire:navigate>
+                {{ __('Categorie') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
