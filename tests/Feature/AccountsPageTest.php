fd67e989e96cbecf0e9e66eb0eda6623bca3e9c5
Daniele Checchin <danielechecchin26@gmail.com>
2026-06-11 09:21:16 +0200

    Test Create Account flow

--- tests/Feature/AccountsPageTest.php
+++ tests/Feature/AccountsPageTest.php
@@ -8,6 +8,7 @@ use App\Livewire\Accounts\Index;
 use App\Models\Account;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class AccountsPageTest extends TestCase
@@ -46,7 +47,79 @@ class AccountsPageTest extends TestCase
             ->assertOk()
             ->assertSeeLivewire(Create::class)
             ->assertSeeText('Create Account')
-            ->assertSeeText('Account creation content will be added here.');
+            ->assertSeeText('Name')
+            ->assertSeeText('Initial Balance')
+            ->assertSeeText('Cancel');
+    }
+
+    public function test_account_can_be_created_for_the_authenticated_user(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        Livewire::test(Create::class)
+            ->set('name', 'Checking Account')
+            ->set('initial_balance', '125.50')
+            ->call('save')
+            ->assertHasNoErrors()
+            ->assertRedirect(route('accounts.index', absolute: false));
+
+        $this->assertDatabaseHas('accounts', [
+            'user_id' => $user->id,
+            'name' => 'Checking Account',
+            'initial_balance' => 125.50,
+        ]);
+    }
+
+    public function test_initial_balance_is_optional_when_creating_an_account(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        Livewire::test(Create::class)
+            ->set('name', 'Cash')
+            ->call('save')
+            ->assertHasNoErrors()
+            ->assertRedirect(route('accounts.index', absolute: false));
+
+        $this->assertDatabaseHas('accounts', [
+            'user_id' => $user->id,
+            'name' => 'Cash',
+            'initial_balance' => null,
+        ]);
+    }
+
+    public function test_account_creation_requires_valid_input(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        Livewire::test(Create::class)
+            ->set('name', '')
+            ->set('initial_balance', 'invalid')
+            ->call('save')
+            ->assertHasErrors([
+                'name' => 'required',
+                'initial_balance' => 'numeric',
+            ])
+            ->assertNoRedirect();
+
+        $this->assertDatabaseCount('accounts', 0);
+    }
+
+    public function test_initial_balance_cannot_have_more_than_two_decimal_places(): void
+    {
+        $this->actingAs(User::factory()->create());
+
+        Livewire::test(Create::class)
+            ->set('name', 'Checking Account')
+            ->set('initial_balance', '12.345')
+            ->call('save')
+            ->assertHasErrors(['initial_balance' => 'decimal'])
+            ->assertNoRedirect();
+
+        $this->assertDatabaseCount('accounts', 0);
     }
 
     public function test_accounts_edit_page_is_displayed(): void
